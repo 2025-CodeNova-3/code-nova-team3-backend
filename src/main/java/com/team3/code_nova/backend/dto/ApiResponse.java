49e6f74bc226e280ef89acaa8d088cddbf2753a2
kisusu <kikisu115@naver.com>
2025-01-02 15:36:41 +0900

    feat: 제네릭 responseDTO 구현

---
+++ src/main/java/com/team3/code_nova/backend/dto/ApiResponse.java
@@ -0,0 +1,12 @@
+package com.team3.code_nova.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private int code;
+    private String message;
+    private T data;
+}
